b9322478150d2da58c01d1f172241b17fcd9f750
Crusader-RG <justin.leeper92@gmail.com>
2024-05-31 15:03:11 -0600

    Pythons complete

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,13 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    ans_list = []
+    for n in range(numbers + 1):
+        jNum = 0
+        lst = [int(i) for i in str(n)]
+        for j in lst:
+            jNum += (j ** len(lst))
+        if n == jNum:
+            ans_list.append(n)    
+    return ans_list   
+
+print(find_armstrong_numbers(0))
--- python/armstrong_numbers_spec.py
+++ python/armstrong_numbers_spec.py
@@ -1,7 +1,17 @@
 # The code is written as driver code. Can you convert it to Unit Test?
 from armstrong_numbers import find_armstrong_numbers
 
-print(find_armstrong_numbers([0]) == [0]) 
-print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
-print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+def test_armstrong_numbers():
+    assert find_armstrong_numbers(0) == [0]
+    assert find_armstrong_numbers(2) == [0,1,2]
+    assert find_armstrong_numbers(9) == [0,1,2,3,4,5,6,7,8,9]
+    assert find_armstrong_numbers(55) == [0,1,2,3,4,5,6,7,8,9]
+    assert find_armstrong_numbers(999) == [0,1,2,3,4, 5, 6, 7, 8, 9, 153, 370, 371, 407]
+
+
+
+
+# print(find_armstrong_numbers(0) == [0]) 
+# print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
+# print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
